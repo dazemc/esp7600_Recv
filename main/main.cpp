b84c1c9f79f0753919b2fdfa8848ee8a347852cd
Daazed J McFarland <daazedjmcfarland@gmail.com>
2026-08-19 14:18:27 -0700

    clean up main

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,4 @@
-#include "display.h"
-#include "events.h"
-#include "lora.h"
-#include "serial.h"
-#include <Arduino.h>
-#include <LoRa.h>
-#include <ssd1306.h>
-#include <string>
+#include "main.h"
 
 QueueHandle_t loraRXQueue = nullptr;
 QueueHandle_t loraTXQueue = nullptr;
@@ -14,29 +7,40 @@ QueueHandle_t serialQueue = nullptr;
 
 static DisplayData displayData{};
 
-extern "C" void app_main() {
+void mainInit() {
   initArduino();
   initSerial();
   ssd1306_handle_t disp = initDisplay();
   initLoRa();
-
-  loraRXQueue = xQueueCreate(10, sizeof(Event));
-  loraTXQueue = xQueueCreate(10, sizeof(Event));
-  displayQueue = xQueueCreate(10, sizeof(Event));
-  serialQueue = xQueueCreate(10, sizeof(Event));
-  if (loraRXQueue == nullptr || displayQueue == nullptr ||
-      serialQueue == nullptr || loraTXQueue == nullptr) {
-    Serial.println("Failed to create event queue");
-    abort();
-  }
-
   int OledX = 0;
   int OledY = 0;
   displayData.disp = disp;
   displayData.OledX = OledX;
   displayData.OledY = OledY;
+  initQueue();
+}
 
+void initQueue() {
+  loraRXQueue = xQueueCreate(10, sizeof(Event));
+  loraTXQueue = xQueueCreate(10, sizeof(Event));
+  displayQueue = xQueueCreate(10, sizeof(Event));
+  serialQueue = xQueueCreate(10, sizeof(Event));
+
+  if (loraRXQueue == nullptr || displayQueue == nullptr ||
+      serialQueue == nullptr || loraTXQueue == nullptr) {
+    Serial.println("Failed to create event queue");
+    abort();
+  }
+}
+
+void initTasks() {
   xTaskCreate(receiveLoRaTask, "RX LoRa", 4096, NULL, 3, NULL);
   xTaskCreate(displayTask, "display", 4096, &displayData, 2, NULL);
   xTaskCreate(printSerialTask, "serial", 4096, NULL, 2, NULL);
+}
+
+extern "C" void app_main() {
+  mainInit();
+  initQueue();
+  initTasks();
 }
---
+++ main/main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "display.h"
+#include "events.h"
+#include "lora.h"
+#include "serial.h"
+#include <Arduino.h>
+#include <LoRa.h>
+#include <ssd1306.h>
+
+extern QueueHandle_t loraRXQueue;
+extern QueueHandle_t loraTXQueue;
+extern QueueHandle_t displayQueue;
+extern QueueHandle_t serialQueue;
+
+void mainInit();
+void initQueue();
